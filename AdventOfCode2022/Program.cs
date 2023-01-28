7ca506a4f3df61e7ed70159f73154bdd64783155
Sam Baker <sambaker@f2s.com>
2023-01-28 15:46:46 +0000

    Day 1 Complete

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode2022
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var lines = await File.ReadAllLinesAsync("Day1.txt");
+
+            var totals = new List<int>();
+            var total = 0;
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    totals.Add(total);
+                    total = 0;
+                    continue;
+                }
+
+                total += int.Parse(line);
+            }
+
+            Console.WriteLine(totals.Max());
+            Console.WriteLine(totals.OrderByDescending(t => t).Take(3).Sum());
+            Console.Read();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AdventOfCode2022
+
+Wherein I do some of the AdventOfCode puzzles from 2022, scrappily.
